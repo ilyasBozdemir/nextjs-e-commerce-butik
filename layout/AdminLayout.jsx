bf15f273e229d4903f7f1d80700fb2b9ef607b89
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-18 17:13:19 +0300

    updated

--- layout/AdminLayout.jsx
+++ layout/AdminLayout.jsx
@@ -1,12 +1,8 @@
-import Head from "next/head";
 import React from "react";
 
 function AdminLayout({ children }) {
   return (
     <>
-      <Head>
-        <title>My Admin App</title>
-      </Head>
       <>{children}</>
     </>
   );
--- layout/UserLayout.jsx
+++ layout/UserLayout.jsx
@@ -1,12 +1,8 @@
-import Head from 'next/head'
 import React from 'react'
 
 function UserLayout({ children }) {
   return (
     <>
-     <Head>
-        <title>My user App</title>
-      </Head>
       <>
         {children}
       </>
