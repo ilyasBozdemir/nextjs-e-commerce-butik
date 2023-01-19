6bd581e513a26fda1ce2759bfe42238ea1835bfe
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-19 12:08:05 +0300

    updated

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -35,12 +35,14 @@ function Header({ onOpen, ...rest }) {
         position="sticky"
         top="0"
         zIndex="250"
-        boxShadow={`rgba(0, 0, 0, 0.07) 0px 1px 1px,
+        _light={{
+          bg: "white",
+          boxShadow: `rgba(0, 0, 0, 0.07) 0px 1px 1px,
         rgba(0, 0, 0, 0.07) 0px 2px 2px,
         rgba(0, 0, 0, 0.07) 0px 4px 4px,
         rgba(0, 0, 0, 0.07) 0px 8px 8px,
-        rgba(0, 0, 0, 0.07) 0px 16px 16px`}
-        _light={{ bg: "white" }}
+        rgba(0, 0, 0, 0.07) 0px 16px 16px`,
+        }}
         _dark={{ bg: "blackAlpha.50" }}
         {...rest}
       >
--- components/SettingSidebar/SettingSidebarButton.jsx
+++ components/SettingSidebar/SettingSidebarButton.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
-function SettingSidebarButton() {
+import React from "react";
+import { IconButton } from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
+function SettingSidebarButton({ onOpen }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <IconButton
+        onClick={onOpen}
+        aria-label="settings button"
+        icon={<SettingsIcon />}
+        size="xl"
+        bg="transparent"
+      />
+    </>
+  );
 }
 
-export default SettingSidebarButton
+export default SettingSidebarButton;
