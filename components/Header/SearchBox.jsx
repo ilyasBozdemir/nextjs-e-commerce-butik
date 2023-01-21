a1a1bcd4492ab8f44097d7d6754b2df93f5d40bf
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-21 11:28:19 +0300

    updated

---
+++ components/Header/SearchBox.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+import Link from "next/link";
+
+import { encode, decode } from "html-entities";
+
+import {
+  Stack,
+  Box,
+  InputGroup,
+  InputRightElement,
+  Input,
+  IconButton,
+  Icon,
+  Flex,
+  Text,
+  Badge,
+} from "@chakra-ui/react";
+import styles from "./index.module.css";
+
+import { AiOutlineClose } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
+import { Highlight } from "@chakra-ui/react";
+
+import { data } from "../../constants/categoryData";
+
+function SearchBox(props) {
+  const [searchHistory, setSearchHistory] = React.useState([
+    { id: 1, name: "Elbise", to: "/elbise/" },
+    { id: 2, name: "trençkot", to: "/trenckot/" },
+    { id: 3, name: "kaban", to: "/kaban/" },
+  ]);
+
+  const [result, setResult] = React.useState(false);
+
+
+  const [closeButtonShow, setCloseButtonShow] = React.useState(false);
+
+  const [inputValue, setInputValue] = React.useState(
+
+  );
+  const isTyping = inputValue.replace(/\s+/, "").length > 0;
+
+  const searchRef = React.useRef();
+
+  React.useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (e) => {
+    if (searchRef.current && !searchRef.current.contains(e.target)) {
+      setInputValue("");
+    }
+  };
+
+  React.useEffect(() => {
+    const filteredResult = data.filter((item) =>
+      item.title.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setResult(filteredResult.length > 0 ? filteredResult : false);
+  }, [inputValue]);
+
+  React.useEffect(() => {
+    if (inputValue.length > 0) {
+      setCloseButtonShow(true);
+    } else {
+      setCloseButtonShow(false);
+    }
+  }, [inputValue]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue !== "") navigate(`/ara?q=${encode(inputValue)}`);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+    if (e.keyCode === 38) {
+      console.log("yukarı");
+    }
+    if (e.keyCode === 40) {
+      console.log("aşağı");
+    }
+  };
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <form autocomplete="off" onSubmit={handleSubmit}></form>
+    </>
+  );
+}
+
+export default SearchBox;
--- pages/404.jsx
+++ pages/404.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
 
-function Page404() {
+import {
+  Box,
+  Button,
+  Flex,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import Logo from "../components/Logo";
+import SearchBox from "../components/Header/SearchBox";
+
+function Page404(props) {
+  const { from, to } = props;
   return (
-    <div>
-      404
-    </div>
-  )
+    <>
+      <Stack
+        spacing={3}
+        w={{ base: "container.xs", lg: "container.lg" }}
+        h={{ base: "container.xs", lg: "container.lg" }}
+      >
+        <Box>
+          <Flex
+            textAlign={{ base: "center", lg: "flex-start" }}
+            justifyContent={{ base: "center", lg: "flex-start" }}
+          >
+            <Link to="/">
+              <Logo />
+            </Link>
+          </Flex>
+        </Box>
+        <Flex
+          direction={"column"}
+          textAlign={"center"}
+          justifyContent={"center"}
+        >
+          <Text fontSize="6xl">404</Text>
+          <Text fontSize="sm">Aradığın içeriğe ulaşamadık.</Text>
+          <Box display={"center"} justifyContent={"center"} mt={3}>
+            <SearchBox w={400} />
+          </Box>
+          <Box>
+            <>
+              <>
+                <Button
+                  as={Link}
+                  to={to || "/"}
+                  mt={3}
+                  w={250}
+                  fontSize={18}
+                  variant="primary"
+                  color={"white"}
+                  bg={useColorModeValue("gray.100", "gray.700")}
+                  bgGradient={"linear(to-l, #7928CA, #FF0080)"}
+                  opacity={2}
+                >
+                  Alışverişe devam et
+                </Button>
+              </>
+            </>
+          </Box>
+        </Flex>
+      </Stack>
+    </>
+  );
 }
 
-export default Page404
+export default Page404;
