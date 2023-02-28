9dd7c2fd57078cbab6af49df290799854aa2427a
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2023-02-28 11:50:52 +0300

    updated

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -7,9 +7,14 @@ function AdminPage() {
       <Head>
         <meta name="robots" content="noindex" />
         <title>Kontrol Paneli</title>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"/>
+        <link rel="shortcut icon" href="/favicon.ico"/>
+        
+
       </Head>
       <>
-      
+
       </>
     </>
   );
